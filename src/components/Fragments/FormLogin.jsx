08fb4be104478f5478269e4ac01652d8ed56df93
ihsan <muhammadihsanansory@gmail.com>
2024-02-14 09:48:53 +0700

    yo

--- src/components/Fragments/FormLogin.jsx
+++ src/components/Fragments/FormLogin.jsx
@@ -1,20 +1,26 @@
 import Button from "../Elements/Button";
 import InputForm from "../Elements/Input";
+import { login } from "../../services/auth.service";
 
 const FormLogin = () => {
   const handleLogin = (event) => {
     event.preventDefault();
-    localStorage.setItem("email", event.target.email.value);
-    localStorage.setItem("password", event.target.password.value);
-    window.location.href = "/products";
+    // localStorage.setItem("email", event.target.email.value);
+    // localStorage.setItem("password", event.target.password.value);
+    // window.location.href = "/products";
+    const data = {
+      username: event.target.username.value,
+      password: event.target.password.value,
+    };
+    login(data);
   };
   return (
     <form onSubmit={handleLogin}>
       <InputForm
-        label="Email"
-        name="email"
-        type="email"
-        placeholder="example@mail.com"
+        label="Username"
+        name="username"
+        type="username"
+        placeholder="Enter username..."
       />
       <InputForm
         label="Password"
